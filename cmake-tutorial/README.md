471da27b02bbb4da4ed4db2d61ec82c6979ece76
Daniate <daniate@126.com>
2019-09-12 10:52:10 +0800

    Step 1: A Basic Starting Point & Adding a Version Number and Configured Header File

---
+++ cmake-tutorial/README.md
@@ -0,0 +1,6 @@
+# CMake Tutorial
+
+[https://cmake.org/cmake-tutorial/](https://cmake.org/cmake-tutorial/)
+
+每个 commit，都对应其中的一个 step。
+
---
+++ cmake-tutorial/TutorialConfig.h
@@ -0,0 +1,10 @@
+/**
+ * 配置选项及设置
+ * 
+ * 1 及 0，
+ * 将会被 CMakeLists.txt 中的内容替换掉。
+ * 
+ * 该文件(TutorialConfig.h.in)的内容将会被写入到对应的头文件中。
+ */
+#define Tutorial_VERSION_MAJOR 1
+#define Tutorial_VERSION_MINOR 0
